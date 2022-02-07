847e99dd3beabfd649f5d61e4840550258f33e1b
Manishav233 <66354254+Manishav233@users.noreply.github.com>
2022-02-07 12:57:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.body.innerHTML=`<section class="makeup-list"> </section>`;
+
+async function getAllItems(){
+const data=await fetch("https://617062da23781c0017289a16.mockapi.io/makeup",{method:"GET"});
+ const items=await data.json();
+const prod =document.querySelector(".makeup-list");
+prod.innerHTML="";
+items.forEach((item)=>{
+prod.innerHTML+=  `
+<div class="images">
+<img src=${item.image_link} alt=${item.name}/>
+
+<div class="details">
+<h3><b>BRAND: </b>${item.brand}</h3>
+<p><b>NAME: </b>${item.name}</p>
+<p><b>PRICE: </b>${item.price}</p>
+<p><b>PRODUCT LINK: </b>${item.product_link}</p>
+<p><b>DESCRIPTION: </b>${item.description}</p>
+</div>
+</div>
+
+`;
+console.log(item.name);
+
+});
+}
+
+getAllItems();
+
+function DeleteAllItems{
+}
