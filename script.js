532fd5e1ce3ac4f6ecbdf004e87c90e5b1f3a95b
Manishav233 <manishav233@gmail.com>
2022-02-08 08:01:47 +0530

    updated delete,post

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-document.body.innerHTML=`<section class="makeup-list"> </section>`;
+document.body.innerHTML=`
+<div class="add-container">
+<input class="Itempos" placeholder="Enter Posture">
+<input class="Itembrand" placeholder="Enter Brand">
+<input class="Itemname" placeholder="Enter Name">
+<input class="Itemlink" placeholder="Enter Product Link">
+<input class="Itemdesc" placeholder="Enter Description">
+<button onclick="addAllItems()">Save</button>
+</div>
+<section class="makeup-list"> </section>`;
 
 async function getAllItems(){
 const data=await fetch("https://617062da23781c0017289a16.mockapi.io/makeup",{method:"GET"});
@@ -17,7 +26,10 @@ prod.innerHTML+=  `
 <p><b>PRICE: </b>${item.price}</p>
 <p><b>PRODUCT LINK: </b>${item.product_link}</p>
 <p><b>DESCRIPTION: </b>${item.description}</p>
+<button onclick="deleteAllItems(${item.id})">DELETE</button>
 </div>
+
+
 </div>
 
 `;
@@ -28,5 +40,24 @@ console.log(item.name, item.price);
 
 getAllItems();
 
-function DeleteAllItems(){
+async function deleteAllItems(item_id){
+console.log(item_id)
+const data=await fetch("https://617062da23781c0017289a16.mockapi.io/makeup/"+item_id,{method:"DELETE"});
+getAllItems();
+}
+
+
+async function addAllItems(){
+const Ipos=document.querySelector(".Itempos").value;
+const Ibrand=document.querySelector(".Itembrand").value;
+const Iname=document.querySelector(".Itemname").value;
+const Ilink=document.querySelector(".Itemlink").value;
+const Idesc=document.querySelector(".Itemdesc").value;
+//console.log(Ipos,Ibrand,Iname,Ilink,Idesc);
+const data=await fetch("https://617062da23781c0017289a16.mockapi.io/makeup",
+{method:"POST",
+headers:{"Content-type":"application/json"},
+body:JSON.stringify({image_link:Ipos,brand:Ibrand,name:Iname,product_link:Ilink,description:Idesc})});
+getAllItems();
+
 }
